7a1a6852a0b13d2c2ef8bec00c82bef3fa4235d8
Zefir Kirilov <contact@zefir.pro>
2024-02-22 14:52:58 +0200

    add default error pages

--- src/main/java/ws/haste/front/WebServer.java
+++ src/main/java/ws/haste/front/WebServer.java
@@ -5,7 +5,14 @@ import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.HttpServerResponse;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
@@ -17,6 +24,7 @@ public class WebServer {
         this.config = config;
         this.server = Front.vertx.createHttpServer();
 
+        registerDefaultErrorPages();
         server.requestHandler(this::requestListener);
         server.listen(config.port());
         Front.getLogger().info("Listening on port " + config.port());
@@ -65,11 +73,7 @@ public class WebServer {
     private void sendError(final @NotNull HttpServerRequest req, final int status) {
         final @NotNull HttpServerResponse res = req.response();
         final @NotNull Optional<@NotNull ErrorResource> errorResource = Optional.ofNullable(config.errorResources().get(status));
-        if (errorResource.isEmpty()) {
-            res.setStatusCode(500);
-            res.headers().set("Content-Type", "text/plain");
-            res.end("500");
-        }
+        if (errorResource.isEmpty()) sendFailSafeServerError(req);
         else {
             res.setStatusCode(status);
             res.headers().set("Content-Type", errorResource.get().contentType);
@@ -77,10 +81,21 @@ public class WebServer {
                 errorResource.get().serve(req);
             }
             catch (final @NotNull Throwable ignored) {
-                res.setStatusCode(500);
-                res.headers().set("Content-Type", "text/plain");
-                res.end("500");
+                sendFailSafeServerError(req);
             }
+        }
+    }
+    
+    private void sendFailSafeServerError(final @NotNull HttpServerRequest req) {
+        final @NotNull HttpServerResponse res = req.response();
+        try {
+            final @NotNull ErrorResource e500 = Objects.requireNonNull(config.errorResources().get(500));
+            e500.serve(req);
+        }
+        catch (final @NotNull WebServerException ignored) {
+            res.setStatusCode(500);
+            res.headers().set("Content-Type", "text/plain");
+            res.end("500");
         }
     }
     private void sendError(final @NotNull HttpServerRequest req, final @NotNull WebServerException error) {
@@ -116,6 +131,43 @@ public class WebServer {
         }
         catch (final @NotNull NumberFormatException e) {
             return Optional.empty();
+        }
+    }
+
+    private void registerDefaultErrorPages() {
+        final int @NotNull [] statuses = {404, 500};
+        for (final int status : statuses) {
+            if (!config.errorResources().containsKey(status)) {
+                final @NotNull File tempFile;
+                try {
+                    tempFile = File.createTempFile(String.valueOf(status), ".html");
+                    tempFile.deleteOnExit();
+                } catch (final @NotNull IOException e) {
+                    Front.getLogger().error("Failed to create temporary file for default " + status + " error page", e);
+                    return;
+                }
+                try (final @NotNull InputStream inputStream = Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("error/" + status + ".html"));
+                     final @NotNull OutputStream outputStream = new FileOutputStream(tempFile)) {
+
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    while ((length = inputStream.read(buffer)) > 0) {
+                        outputStream.write(buffer, 0, length);
+                    }
+                }
+                catch (final @NotNull IOException e) {
+                    Front.getLogger().error("Failed to write to temporary file for default " + status + " error page", e);
+                    return;
+                }
+                config.errorResources().put(status, new ErrorResource(
+                        "text/html",
+                        new HashMap<>() {{
+                            put(FileResource.Encoding.Identity, tempFile);
+                        }},
+                        null,
+                        null
+                ));
+            }
         }
     }
 }
