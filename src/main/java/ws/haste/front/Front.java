759e644ae22f1a84d8df189659805e9f4bbe0459
Zefir Kirilov <contact@zefir.pro>
2024-02-23 12:26:53 +0200

    find config method

--- src/main/java/ws/haste/front/Front.java
+++ src/main/java/ws/haste/front/Front.java
@@ -7,6 +7,8 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Optional;
 
 /**
  * Main class
@@ -39,4 +41,20 @@ public class Front {
 
     public final static @NotNull Vertx vertx = Vertx.vertx();
     private static final @NotNull Logger logger = LogManager.getLogger(Front.class);
+
+    private static @NotNull Config findConfig() throws Config.ConfigException {
+        final @NotNull Optional<@NotNull String> env = Optional.ofNullable(System.getenv("HASTE_CONFIG"));
+        if (env.isPresent()) return Config.fromYaml(env.get());
+
+        final @NotNull HashSet<@NotNull String> locations = new HashSet<>(Arrays.asList(
+                "haste.yaml",
+                "haste.yml",
+                "/etc/haste/config.yaml",
+                "/etc/haste/config.yml"
+        ));
+        for (final @NotNull String location : locations)
+            if (new File(location).exists()) return Config.fromYaml(location);
+        getLogger().warn("Did not find configuration file, using default config");
+        return Config.fromYaml("haste://config.yaml");
+    }
 }
